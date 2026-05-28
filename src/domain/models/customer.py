f7d9968535757ac6d16f51a7cbbed54dcc0894c1
mitch <github@walkermitch.com>
2026-05-28 21:03:04 +1000

    fix: update relative import in customer.py to absolute import

--- src/domain/models/customer.py
+++ src/domain/models/customer.py
@@ -1,4 +1,4 @@
-from user import User
+from src.domain.models.user import User
 class Customer(User): 
     def __init__(self,name, address, phone_number, password):
         super().__init__(name, address, phone_number, password)

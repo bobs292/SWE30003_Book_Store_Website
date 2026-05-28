5aa85aeab6ac0bc4d3225b835a558c349b74c252
mitch <github@walkermitch.com>
2026-05-28 21:04:00 +1000

    fix: rename catalogue route function to avoid conflict with blueprint name

--- src/presentation/routes/catalogue_routes.py
+++ src/presentation/routes/catalogue_routes.py
@@ -3,5 +3,5 @@ from flask import Blueprint, render_template
 catalogue = Blueprint('catalogue', __name__)
 
 @catalogue.route('/catalogue')
-def catalogue():
+def catalogue_page():
     return render_template('catalogue.html')
